21688d15971c508e855c168066832fc39854f792
andiharun29 <andiharunalrasyid14@gmail.com>
2023-04-14 15:46:11 +0700

    memasukkan berapa banyak eleme yang akan di masukkan

--- liearsearch/liearsearch.cpp
+++ liearsearch/liearsearch.cpp
@@ -6,5 +6,17 @@ int n;
 int i;
 
 void input()
+{
+	while (true)
+	{
+		cout << "enter the number of element in the array : ";
+		cin >> n;
+		if ((n > 0) && (n <= 20))
+			break;
+		else
+			cout << "\narray should have minimum 1 and maximum 20 element.\n\n";
+	}
+
 
 	
+
