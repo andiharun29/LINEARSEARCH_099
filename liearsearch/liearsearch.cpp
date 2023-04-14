fc51b6b87bbd790ba7bbba67df577ea871904a0a
andiharun29 <andiharunalrasyid14@gmail.com>
2023-04-14 15:44:11 +0700

    membuat array

--- liearsearch/liearsearch.cpp
+++ liearsearch/liearsearch.cpp
@@ -1,20 +1,70 @@
-// liearsearch.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
+
+int arr[20];
+int n;
+int i;
+
+void input()
+{
+	while (true)
+	{
+		cout << "enter the number of element in the array : ";
+		cin >> n;
+		if ((n > 0) && (n <= 20))
+			break;
+		else
+			cout << "\narray should have minimum 1 and maximum 20 element.\n\n";
+	}
+
+
+	cout << "\n---------------------\n";
+	cout << " enter array element \n";
+	cout << "---------------------\n";
+	for (i = 0; i < n; i++)
+	{
+		cout << "<" << (i + 1) << "> ";
+		cin >> arr[i];
+	}
+}
+
+
+
+void linesearch()
+{
+	char ch;
+	int comparisons;
+
+	do
+	{
+		cout << "\nenter the element you want to search: ";
+		int item;
+		cin >> item;
+
+		comparisons = 0;
+		for (i = 0; i < n; i++)
+		{
+			comparisons++;
+			if (arr[i] == item)
+			{
+				cout << "\n" << item << " found at position " << (i + 1) << endl;
+				break;
+			}
+
+		}
+
+		if (i == n)
+			cout << "\n" << item << "not found in the array\n";
+		cout << "\nnumber of comparisons: " << comparisons << endl;
+
+		cout << "\ncontinue search (y/n): ";
+		cin >> ch;
+	} while ((ch == 'y') || (ch == 'Y'));
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	input();
+	linesearch();
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
