f21e7d16bfc3fde384b02add2a73d2e8ff79927d
andiharun29 <andiharunalrasyid14@gmail.com>
2023-04-14 15:48:21 +0700

    masukkan elemennya

--- liearsearch/liearsearch.cpp
+++ liearsearch/liearsearch.cpp
@@ -18,5 +18,15 @@ void input()
 	}
 
 
-	
+	cout << "\n---------------------\n";
+	cout << " enter array element \n";
+	cout << "---------------------\n";
+	for (i = 0; i < n; i++)
+	{
+		cout << "<" << (i + 1) << "> ";
+		cin >> arr[i];
+	}
+}
+
+
 
